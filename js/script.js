2246e4d03268d0da47b23904264b1202c9a2561a
elem3d <elem3d@users.noreply.github.com>
2026-09-25 23:19:21 -0300

    feat: dinamizar formulário de onboarding peo script.js, criar obj user

--- js/script.js
+++ js/script.js
@@ -1,22 +1,49 @@
 const formulario = document.getElementById("formulario");
+const generosContainer = document.querySelector(".generos-container")
+const generosList = document.getElementsByName("generos")
 
+let generosMarcados = [];
+
+generosList.forEach((gen) => gen.addEventListener("change", (event) =>{
+    if (event.target.checked) {
+        generosMarcados.push(event.target.value)
+        console.log(generosMarcados)
+    }
+    else{
+        const posicao = generosMarcados.indexOf(event.target.value);
+        generosMarcados.splice(posicao, 1);
+        console.log(generosMarcados)
+    }
+    
+
+}))
 
 formulario.addEventListener("submit", function(event) {
 
     event.preventDefault();
 
+    if(generosMarcados.length > 5){
+        const mensagemErro = document.createElement("p");
+        mensagemErro.textContent = "Selecione apenas 5 gêneros"
+        mensagemErro.classList.add("gen-erro-msg")
+        generosContainer.appendChild(mensagemErro)
+        return
+    }
+
     const nome = document.getElementById("nome").value;
     const idade = document.getElementById("idade").value;
-    //const generos = document.getElementById("genero").value;
+    const generosFav = generosMarcados;
 
     const usuario = {
         nome: nome,
         idade: idade,
-        genero: genero
+        generos: generosFav
     };
 
+    console.log(usuario)
     localStorage.setItem("usuario", JSON.stringify(usuario));
 
-    alert("Dados salvos com sucesso!");
-
+    window.location.href = "../catalogo.html";
+    
+    renderizarCatalogo();
 });
